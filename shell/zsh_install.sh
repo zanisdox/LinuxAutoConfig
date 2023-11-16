1247af2c6b62b5cc35b8700983190019ae2adb38
zanisdox <chien.do@zanis.dev>
2023-11-16 10:54:53 +0700

    Edit zsh fonts

--- shell/zsh_install.sh
+++ shell/zsh_install.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-for dir in *; do [[ -d "$dir" ]] && { user=$dir; break; }; done
-
+# OhMyZSH
+sudo dnf install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# rm -rf MesloLGS_NF
-# mkdir MesloLGS_NF
-# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O MesloLGS_NF/MesloLGS_NF_Regular.ttf
-# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O MesloLGS_NF/MesloLGS_NF_Bold.ttf
-# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -O MesloLGS_NF/MesloLGS_NF_Italic.ttf
-# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O MesloLGS_NF/MesloLGS_NF_Bold_Italic.ttf
-# sudo cp -r MesloLGS_NF /usr/share/fonts
-# sudo fc-cache -v
-# rm -rf MesloLGS_NF
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh-custom/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh-custom/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh-custom/powerlevel10k
-echo 'source ~/.zsh-custom/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
-echo 'source ~/.zsh-custom/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
-echo 'source ~/.zsh-custom/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Extension Install
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.ohmyzshExtensions/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.ohmyzshExtensions/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.ohmyzshExtensions/powerlevel10k
+echo 'source ~/.ohmyzshExtensions/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+echo 'source ~/.ohmyzshExtensions/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+echo 'source ~/.ohmyzshExtensions/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Fonts Downloads
+fonts = '/usr/share/fonts'
+mkdir tempFonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -P tempFonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/DejaVuSansMono.zip -P tempFonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/ComicShannsMono.zip -P tempFonts
+sudo unzip tempFonts/Hack.zip -d $fonts/Hack
+sudo unzip tempFonts/DejaVuSansMono.zip -d $fonts/DejaVuSansMono
+sudo unzip tempFonts/ComicShannsMono.zip -d $fonts/ComicShannsMono
+rm -rf tempFonts
+sudo fc-cache -v
+
+# Change User Shell
 chsh -s $(which zsh)
